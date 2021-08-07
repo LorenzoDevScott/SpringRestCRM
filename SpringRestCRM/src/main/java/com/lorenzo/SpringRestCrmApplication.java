74b0e90593378b8fff0a2af2a612a97a633e42ff
Lorenzo Scott <lscott1220@gmail.com>
2021-08-07 15:26:42 -0700

    Starter Project For REST CRM

---
+++ SpringRestCRM/src/main/java/com/lorenzo/SpringRestCrmApplication.java
@@ -0,0 +1,13 @@
+package com.lorenzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCrmApplication.class, args);
+	}
+
+}
