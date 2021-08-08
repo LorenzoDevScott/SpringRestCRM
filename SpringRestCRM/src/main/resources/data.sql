32c1635468242bfde78a0b0a205f56d9777ae557
Lorenzo Scott <lscott1220@gmail.com>
2021-08-07 22:48:32 -0700

    Added Test Data.
    Test data must be run manually every time in DataGrip.

---
+++ SpringRestCRM/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into SpringRestCRM.ticket values(1, "broke phone", "screen repair");
+insert into SpringRestCRM.ticket values(2, "dropped phone", "screen repair");
+insert into SpringRestCRM.ticket values(3, "dropped phone in toilet", "water damage");
+insert into SpringRestCRM.ticket values(4, "dropped phone in lake", "water damage");
