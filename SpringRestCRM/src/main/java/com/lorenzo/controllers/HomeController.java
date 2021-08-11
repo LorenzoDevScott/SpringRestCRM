c63b28d5fa2c5fb1cb5c011b9333b3345e31007a
Lorenzo Scott <lscott1220@gmail.com>
2021-08-11 13:28:21 -0700

    Created Get Mappings For Ticket
    Find all tickets
    Find all tickets with certain repair type

--- SpringRestCRM/src/main/java/com/lorenzo/controllers/HomeController.java
+++ SpringRestCRM/src/main/java/com/lorenzo/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.lorenzo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lorenzo.TicketServiceImpl;
 import com.lorenzo.models.Ticket;
+import com.lorenzo.serviceimplementation.TicketServiceImpl;
 
 @RestController
 @CrossOrigin("http://localhost:8080/**")
@@ -19,11 +21,27 @@ public class HomeController {
 	@Autowired
 	private TicketServiceImpl ticketServ;
 	
-	@GetMapping("ticket/{id}")
+	// Get Ticket By ID
+	@GetMapping("data/tickets/{id}")
 	public Ticket getTicket(@PathVariable Long id) throws Exception{
 		System.out.println(ticketServ.findById(id));
 		return ticketServ.findById(id);
 	}
+	
+	
+    // Get List of Tickets 
+	@GetMapping("data/tickets/")
+	public List<Ticket> findAll() {
+		return ticketServ.findAll();
+	}
+	
+	// Get List of Ticket by Repair Type
+	@GetMapping("data/tickets/{repairType}")
+	public List<Ticket> getTickets(@PathVariable String repairType) {
+		return ticketServ.findByRepairType(repairType);
+	}
+	
+	
 	
 	
 }
