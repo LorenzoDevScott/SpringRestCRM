48618d8d4e2b97f4fa73663c966282f6a164e483
Lorenzo Scott <lscott1220@gmail.com>
2021-08-12 10:45:58 -0700

    Adding mapping for Customer data

--- SpringRestCRM/src/main/java/com/lorenzo/controllers/HomeController.java
+++ SpringRestCRM/src/main/java/com/lorenzo/controllers/HomeController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lorenzo.models.Customer;
 import com.lorenzo.models.Ticket;
+import com.lorenzo.serviceimplementation.CustomerServiceImpl;
 import com.lorenzo.serviceimplementation.TicketServiceImpl;
 
 @RestController
@@ -21,13 +23,15 @@ public class HomeController {
 	@Autowired
 	private TicketServiceImpl ticketServ;
 	
-	// Get Ticket By ID
-	@GetMapping("data/tickets/{id}")
-	public Ticket getTicket(@PathVariable Long id) throws Exception{
-		System.out.println(ticketServ.findById(id));
-		return ticketServ.findById(id);
-	}
+	@Autowired
+	private CustomerServiceImpl customerServ;
 	
+	// Get Ticket By ID
+	@GetMapping("data/ticket")
+	public Ticket getTicket(@RequestParam(value="tid") Long tid) throws Exception{
+		System.out.println(ticketServ.findById(tid));
+		return ticketServ.findById(tid);
+	}
 	
     // Get List of Tickets 
 	@GetMapping("data/tickets/")
@@ -39,6 +43,12 @@ public class HomeController {
 	@GetMapping("data/tickets/{repairType}")
 	public List<Ticket> getTickets(@PathVariable String repairType) {
 		return ticketServ.findByRepairType(repairType);
+	}
+	
+	@GetMapping("data/customer")
+	public Customer getCustomer(@RequestParam(value="cid") Long cid) throws Exception {
+		System.out.println(customerServ.findById(cid));
+		return customerServ.findById(cid);
 	}
 	
 	
