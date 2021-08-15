e9b4ffb31953f5bd5e6fe417f567456c9291ff10
Lorenzo Scott <lscott1220@gmail.com>
2021-08-15 13:37:10 -0700

    Uncommented test data in the runner class

--- SpringRestCRM/src/main/java/com/lorenzo/Runner.java
+++ SpringRestCRM/src/main/java/com/lorenzo/Runner.java
@@ -32,7 +32,7 @@ public class Runner implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		ticketServ.save(new Ticket("Screen Repair", "Dropped phone on the concrete"));
 		ticketServ.save(new Ticket("Screen Repair", "Tripped and dropped phone on rocks"));
-		ticketServ.save(new Ticket("Water Damange", "Accidentally dropped phone in the toilet"));
+		ticketServ.save(new Ticket("Water Damage", "Accidentally dropped phone in the toilet"));
 		ticketServ.save(new Ticket("Water Damage", "Went swimming in lake and forgot phone was in pocket"));
 		ticketServ.save(new Ticket("OS Install", "Customer bought a new computer and needs Windows 10 installed"));
 		
