af01d5913cf321e8e3095e53f6b46bea3d5a26dd
Lorenzo Scott <lscott1220@gmail.com>
2021-08-22 14:37:49 -0700

    Tested Relationships in Runner Class
    Customer created first, then ticket is created, then ticketupdates were
    created then customer was saved.
    
    Functionality works as intended so far still need to create Junit tests
    to confirm

--- SpringRestCRM/src/main/java/com/lorenzo/Runner.java
+++ SpringRestCRM/src/main/java/com/lorenzo/Runner.java
@@ -8,10 +8,12 @@ import com.lorenzo.models.Customer;
 import com.lorenzo.models.PartOrder;
 import com.lorenzo.models.Product;
 import com.lorenzo.models.Ticket;
+import com.lorenzo.models.TicketUpdate;
 import com.lorenzo.serviceimplementation.CustomerServiceImpl;
 import com.lorenzo.serviceimplementation.PartOrderServiceImpl;
 import com.lorenzo.serviceimplementation.ProductServiceImpl;
 import com.lorenzo.serviceimplementation.TicketServiceImpl;
+import com.lorenzo.serviceimplementation.TicketUpdateServiceImpl;
 
 @Component
 public class Runner implements CommandLineRunner {
@@ -28,13 +30,30 @@ public class Runner implements CommandLineRunner {
 	@Autowired
 	private ProductServiceImpl productServ;
 	
+	@Autowired
+	private TicketUpdateServiceImpl ticketUpdateServ;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		ticketServ.save(new Ticket("Screen Repair", "Dropped phone on the concrete"));
-		ticketServ.save(new Ticket("Screen Repair", "Tripped and dropped phone on rocks"));
-		ticketServ.save(new Ticket("Water Damage", "Accidentally dropped phone in the toilet"));
-		ticketServ.save(new Ticket("Water Damage", "Went swimming in lake and forgot phone was in pocket"));
-		ticketServ.save(new Ticket("OS Install", "Customer bought a new computer and needs Windows 10 installed"));
+		
+		Customer customer = new Customer("Mary","Jay","mjay@gmail.com");
+		
+		// Create the ticket
+		Ticket ticket = new Ticket("Screen Repair", "Droped phone on some rocks", customer);
+		Ticket ticket2 = new Ticket("Water Damage", "Phone slipped out of sweater pocket into toilet", customer);
+		
+		// Create updates
+		TicketUpdate update1 = new TicketUpdate("Diagnosed the phone and realized the camera is also broken", ticket);
+		ticket.getUpdates().add(update1);
+//		ticketServ.save(ticket);
+		
+		TicketUpdate update2 = new TicketUpdate("Opened the phone and found corrosion on multiple componenets", ticket2);
+		ticket2.getUpdates().add(update2);
+//		ticketServ.save(ticket2);
+		customer.getTickets().add(ticket);
+		customer.getTickets().add(ticket2);
+		customerServ.save(customer);
+		
 		
 		customerServ.save(new Customer("John","Doe","johnd@gmail.com"));
 		customerServ.save(new Customer("Jane","Dawson","janed@gmail.com"));
@@ -52,6 +71,12 @@ public class Runner implements CommandLineRunner {
 		productServ.save(new Product("iPhone 13", 999.99 ,27));
 		productServ.save(new Product("iPad M1 2021", 1099.99 ,159));
 		productServ.save(new Product("MacBook Pro M2 2021", 1499.99 ,120));
+		
+		ticketServ.save(new Ticket("Screen Repair", "Dropped phone on the concrete", customerServ.findById((long) 1)));
+		ticketServ.save(new Ticket("Screen Repair", "Tripped and dropped phone on rocks", customerServ.findById((long) 2)));
+		ticketServ.save(new Ticket("Water Damage", "Accidentally dropped phone in the toilet", customerServ.findById((long) 1)));
+		ticketServ.save(new Ticket("Water Damage", "Went swimming in lake and forgot phone was in pocket", customerServ.findById((long) 3)));
+		ticketServ.save(new Ticket("OS Install", "Customer bought a new computer and needs Windows 10 installed", customerServ.findById((long) 4)));
 		
 		
 	}
