079df8ad5fa9cc64af9e600915c1f0554e8c0ff2
Lorenzo Scott <lscott1220@gmail.com>
2021-08-22 14:26:45 -0700

    Added TimeStamp and Customer to Ticket relationship
    Created and Updated timestamps have been created for record keeping in
    the UI.
    
    OneToMany relationship between Customer and Ticket
    List<Ticket> tickets property is annotated with JsonManagedReference
    
    Customer Constructor now instatiates a new arrayList of Tickets
    
    JsonProperty added to rename "cid"

--- SpringRestCRM/src/main/java/com/lorenzo/models/Customer.java
+++ SpringRestCRM/src/main/java/com/lorenzo/models/Customer.java
@@ -1,13 +1,25 @@
 package com.lorenzo.models;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
+@Table(name="customers")
 public class Customer {
 	
 	@Id
@@ -17,38 +29,81 @@ public class Customer {
 	private String lastName;
 	private String email;
 	
+	@CreationTimestamp
+	private LocalDateTime createDateTime;
+	
+	@UpdateTimestamp
+	private LocalDateTime updateDateTime;
+	
+	@OneToMany(mappedBy="customer", cascade = CascadeType.ALL)
+	@JsonManagedReference
+	private List<Ticket> tickets;
+	
 	public Customer() {
-		super();
+	
 	}
 	
 	public Customer(String firstName, String lastName, String email) {
-		super();
+		this();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
+		this.tickets = new ArrayList<>();
 	}
+	
+	@JsonProperty("cid")
 	public Long getId() {
 		return cid;
 	}
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
 
+	public List<Ticket> getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(List<Ticket> tickets) {
+		this.tickets = tickets;
+	}
+
+	public LocalDateTime getCreateDateTime() {
+		return createDateTime;
+	}
+
+	public void setCreateDateTime(LocalDateTime createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+
+	public LocalDateTime getUpdateDateTime() {
+		return updateDateTime;
+	}
+
+	public void setUpdateDateTime(LocalDateTime updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+	
+	
+	
 }
