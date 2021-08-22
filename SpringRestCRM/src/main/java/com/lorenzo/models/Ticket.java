e1ac0da990cfe86889edc5aec5a3794ac3bcfe72
Lorenzo Scott <lscott1220@gmail.com>
2021-08-22 14:21:57 -0700

    Updated Ticket Model to link to TicketUpdate and Customer
    Ticket has a OneToMany relationship with TicketUpdate
    
    There is a JsonManagedReference annotation on the List<TicketUpdate>
    update property
    
    This is going to be serialized into Json based on the TicketUpdate
    property
    
    There is a ManyToOne relationship to Customer
    There can be multiple Tickets attached to a Customer.
    
    JsonBackReference is attached to Customer customer property. This will
    not be referenced in Json.
    
    JsonProperties have been updated

--- SpringRestCRM/src/main/java/com/lorenzo/models/Ticket.java
+++ SpringRestCRM/src/main/java/com/lorenzo/models/Ticket.java
@@ -1,11 +1,24 @@
 package com.lorenzo.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
+@Table(name="tickets")
 public class Ticket {
 	
 	@Id
@@ -14,16 +27,28 @@ public class Ticket {
 	private String repairType;
 	private String description;
 	
+	// One Ticket can have many updates
+	@OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
+	@JsonManagedReference
+	private List<TicketUpdate> updates;
+	
+	@ManyToOne
+	@JsonBackReference
+	private Customer customer;
+	
 	public Ticket() {
-		super();
+
 	}
 
-	public Ticket(String repairType, String description) {
-		super();
+	public Ticket(String repairType, String description, Customer customer) {
+		this();
 		this.repairType = repairType;
 		this.description = description;
+		this.customer = customer;
+		this.updates = new ArrayList<>();
 	}
 	
+	@JsonProperty("tid")
 	public long getId() {
 		return tid;
 	}
@@ -31,13 +56,36 @@ public class Ticket {
 	public String getRepairType() {
 		return repairType;
 	}
+	
 	public void setRepairType(String repairType) {
 		this.repairType = repairType;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+	public List<TicketUpdate> getUpdates() {
+		return updates;
+	}
+
+	public void setUpdates(List<TicketUpdate> updates) {
+		this.updates = updates;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+	
+	
+	
 }
