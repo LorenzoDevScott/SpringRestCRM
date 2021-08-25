4c72edc9b35a294dc37bbff45a65e4c7b776aedb
Lorenzo Scott <lscott1220@gmail.com>
2021-08-25 15:35:46 -0700

    Added create and update timestamps to the TicketUpdate model

--- SpringRestCRM/src/main/java/com/lorenzo/models/TicketUpdate.java
+++ SpringRestCRM/src/main/java/com/lorenzo/models/TicketUpdate.java
@@ -1,5 +1,7 @@
 package com.lorenzo.models;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -21,6 +26,12 @@ public class TicketUpdate {
 	private long tuid;
 	
 	private String ticketUpdate;
+	
+	@CreationTimestamp
+	private LocalDateTime createDateTime;
+	
+	@UpdateTimestamp
+	private LocalDateTime updateDateTime;
 	
 	@ManyToOne
 	@JsonBackReference
@@ -39,10 +50,6 @@ public class TicketUpdate {
 	public long getTuid() {
 		return tuid;
 	}
-	
-	public void setTuid(long tuid) {
-		this.tuid = tuid;
-	}
 
 	@JsonProperty("update")
 	public String getTicketUpdate() {
@@ -59,6 +66,22 @@ public class TicketUpdate {
 
 	public void setTicket(Ticket ticket) {
 		this.ticket = ticket;
+	}
+
+	public LocalDateTime getCreateDateTime() {
+		return createDateTime;
+	}
+
+	public void setCreateDateTime(LocalDateTime createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+
+	public LocalDateTime getUpdateDateTime() {
+		return updateDateTime;
+	}
+
+	public void setUpdateDateTime(LocalDateTime updateDateTime) {
+		this.updateDateTime = updateDateTime;
 	}
 	
 }
